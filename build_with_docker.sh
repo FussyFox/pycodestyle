79f9841b109a0cc364024325716c6fd3dd4af6eb
Johannes Hoppe <info@johanneshoppe.com>
2018-03-03 12:41:25 +0100

    Upgrade docker build

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+set -ex
+
 docker pull lambdalint/apex-builder
-docker run -ti -v $(pwd):/app lambdalint/apex-builder pip install --no-use-wheel -r requirements.txt -t .
+docker run -ti -v $(pwd):/app lambdalint/apex-builder python3 -m pip install --no-use-wheel -r requirements.txt -t .
