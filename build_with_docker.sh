a8027b658918702331f1562ac5c01bb5c8371004
Johannes Hoppe <info@johanneshoppe.com>
2018-03-03 12:07:53 +0100

    Do not use wheels

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 docker pull lambdalint/apex-builder
-docker run -ti -v $(pwd):/app lambdalint/apex-builder pip install -r requirements.txt -t .
+docker run -ti -v $(pwd):/app lambdalint/apex-builder pip install --no-use-wheel -r requirements.txt -t .
