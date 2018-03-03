1de41238ee8e30f6919cf9645eb416636eaa2fb0
Johannes Hoppe <info@johanneshoppe.com>
2018-03-03 15:48:17 +0100

    Manually zip function to include hidden dot-files

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -3,4 +3,5 @@
 set -ex
 
 docker pull lambdalint/apex-builder
-docker run -ti -v $(pwd):/app lambdalint/apex-builder pip-3.6 install -r requirements.txt -t .
+docker run -ti -v "$(pwd):/app" lambdalint/apex-builder pip-3.6 install -r requirements.txt -t .
+zip function.zip -r ./ -x .git -x *.pyc
