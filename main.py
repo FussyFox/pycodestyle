b01ed1ceb654cfb7d41f70a9bd52e13cc6ff5152
Johannes Hoppe <info@johanneshoppe.com>
2018-03-25 19:38:19 +0200

    Add logger

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 """Lambda function that executes pycodestyle, a static file linter."""
+import logging
+import sys
 
 from lintipy import Handler
+
+root_logger = logging.getLogger('')
+root_logger.setLevel(logging.INFO)
+root_logger.addHandler(logging.StreamHandler(sys.stdout))
 
 handle = Handler('PEP8', 'pycodestyle', '.', )
