1efcf8dcb1a0d941ede5ac96efa54396899a8cb6
Johannes Hoppe <info@johanneshoppe.com>
2018-03-25 23:22:41 +0200

    Revert "Use dot_match param in favor of custom zip command"
    This reverts commit dcfa4e507b7c2e81f9a744d108ca4c827e2747bf.

--- build_with_docker.sh
+++ build_with_docker.sh
@@ -4,3 +4,4 @@ set -ex
 
 docker pull lambdalint/lambda-function-builder
 docker run -ti -v "$(pwd):/var/task" lambdalint/lambda-function-builder pip-3.6 install -r requirements.txt -t .
+zip function.zip -r ./ -x *.git/* -x *.pyc -x *.dist-info/* -x *.egg-info/* -x *__pycache__/*
