c399bf8ddea699dc1f97c2d9b363275e520be552
Johannes Hoppe <info@johanneshoppe.com>
2018-03-27 10:57:21 +0200

    Revert "Incease log level to DEBUG"
    This reverts commit 8257fcd2870d4e2598e19c9bc03212082d75d6a9.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 from lintipy import Handler
 
 root_logger = logging.getLogger('')
-root_logger.setLevel(logging.DEBUG)
+root_logger.setLevel(logging.INFO)
 root_logger.addHandler(logging.StreamHandler(sys.stdout))
 
 handle = Handler('PEP8', 'pycodestyle', '.', )
