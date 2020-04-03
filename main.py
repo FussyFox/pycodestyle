588ad8c0d603ac9dafd519a1b7666e081762df71
Johannes Hoppe <info@johanneshoppe.com>
2020-04-03 16:10:52 +0200

    Remove explicit logger setup

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 """Lambda function that executes pycodestyle, a static file linter."""
-import logging
-import sys
-
 from lintipy import CheckRun
-
-root_logger = logging.getLogger('')
-root_logger.setLevel(logging.DEBUG)
-root_logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 handle = CheckRun.as_handler('pycodestyle', 'pycodestyle', '.', )
